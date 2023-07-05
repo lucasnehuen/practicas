d5cd07893ba9083c39d599133bdb0f49b40cfeec
lucasnehuen <you@example.com>
2023-07-05 15:08:56 -0300

    cree un formulario

--- src/app/components/additem/additem.component.ts
+++ src/app/components/additem/additem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-additem',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./additem.component.css']
 })
 export class AdditemComponent implements OnInit {
+  id:number=0;
+  title:string='';
+  price:number=0;
+  quantity:number=0
 
 constructor(){ }
 ngOnInit(): void {
   
+}
+
+onSubmit(){
+  const item = new Item();
+  item.id = this.id;
+  item.title = this.title;
+  item.price = this.price;
+  item.quantity = this.quantity;
+  item.completed = false;
 }
 
 }
